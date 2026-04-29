e687060bb1d735d61e03c8e8e36014b27cfdd707
Enrique Sanchez <enrique.sanchez@futurite.com>
2026-04-29 10:17:10 -0600

    hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [react(), keystatic()],
   vite: {
