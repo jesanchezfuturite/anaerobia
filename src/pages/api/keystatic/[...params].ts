daedbe9760a8a7f53140fb40d5132afbc683c01d
Enrique Sanchez <enrique.sanchez@futurite.com>
2026-04-29 11:33:45 -0600

    Fix Vercel deploy by removing .vercel folder from git and clearing manual routes

--- src/pages/api/keystatic/[...params].ts
+++
@@ -1,7 +0,0 @@
-export const prerender = false;
-import { makeHandler } from '@keystatic/astro/api';
-import keystaticConfig from '../../../../keystatic.config';
-
-export const ALL = makeHandler({
-  config: keystaticConfig,
-});
